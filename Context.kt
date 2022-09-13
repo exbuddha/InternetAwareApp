4ac6018fb46a38c89f1900d6801909e4a8b1e5f6
exbuddha <akamran@gmail.com>
2022-09-13 07:28:21 -0700

    Update Context.kt

--- Context.kt
+++ Context.kt
@@ -3,7 +3,7 @@ var session: AppRuntimeSessionEntity? = null
 val permissions
     get() = app.packageManager.getPackageInfo(app.packageName, GET_PERMISSIONS).requestedPermissions
 
-fun <R> app(block: MediaPlayerApp.() -> R) = app.block()
+fun <R> app(block: InternetAwareApp.() -> R) = app.block()
 
 inline fun <reified R> trySafely(block: () -> R?) = try { block() } catch(_: Throwable) { R::class.objectInstance }
 
